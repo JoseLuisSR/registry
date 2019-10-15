bff9784c875dbe7d06068a6b0b67ccb65bfb03a5
JoseLuisSR <jose.luis.sastoque.rey@gmail.com>
2019-10-14 22:09:12 -0500

    Add vagrant to build virtual machine with docker engine community edition.

---
+++ install.sh
@@ -0,0 +1,16 @@
+# Update package list
+apt-get update
+
+# Update Ubuntu kernel version from 3.13 to 4.4.0, ref: https://wiki.ubuntu.com/Kernel/LTSEnablementStack
+echo vagrant | sudo -S apt-get install -y --install-recommends linux-generic-lts-xenial
+
+# Install docker from a package
+
+# Download .deb file for ubuntu/trusty64
+wget https://download.docker.com/linux/ubuntu/dists/trusty/pool/stable/amd64/docker-ce_18.06.3~ce~3-0~ubuntu_amd64.deb
+
+# Install libsystemd-journal0
+echo vagrant | sudo -S apt-get install -y libsystemd-journal0
+
+# Install docker engine for Community Edition.
+echo vagrant | sudo -S dpkg -i docker-ce_18.06.3~ce~3-0~ubuntu_amd64.deb
